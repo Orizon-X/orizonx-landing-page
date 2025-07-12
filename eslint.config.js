ab5c4c92b322dd3b3d90d3fcd2456f389dea8dea
CaiqueOrtega <dev.caique.ortega@outlook.com>
2025-07-12 16:49:13 -0300

    chore: update ESLint config to ignore coverage directory
    - Added 'coverage' folder to globalIgnores to prevent linting coverage files
    - Removed unused import of 'eslint-plugin-tailwindcss'
    - Ensured ESLint ignores both 'dist' and 'coverage' folders for cleaner lint runs

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
-import tailwindcss from 'eslint-plugin-tailwindcss';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
